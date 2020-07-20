c4fbc6513d42658700eb35e8827942d406d9f290
Lemon Patwari <lemonpatwari@gmail.com>
2020-07-20 20:42:08 +0600

    employee dashboard create

---
+++ components/Dashboard/navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-md-12">
+    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+
+      <nuxt-link to="/dashboard" class="nav-link mb-2" :class="$nuxt.$route.name == 'dashboard' ? 'active' : ''">Home</nuxt-link>
+
+      <nuxt-link to="/edit-resume" class="nav-link mb-2"  :class="$nuxt.$route.name == 'edit-resume' ? 'active' : ''">Edit Resume</nuxt-link>
+      <nuxt-link to="/view-resume" class="nav-link mb-2"  :class="$nuxt.$route.name == 'view-resume' ? 'active' : ''">View Resume</nuxt-link>
+
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/dashboard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="page-content">
+    <div class="container-fluid">
+
+      <div class="row">
+
+        <div class="col-lg-2">
+          <div class="card">
+            <div class="card-body">
+              <div class="row">
+                <dashboardNavbar/>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-10">
+          <div class="card">
+            <div class="card-body">
+              <h1>Dashboard</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, iste!</p>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import dashboardNavbar from '~/components/Dashboard/navbar'
+
+  export default {
+    name: "dashboard",
+    components: {
+      dashboardNavbar
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
