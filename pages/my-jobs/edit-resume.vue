c612e02bf4a5ffb5bfd64142e2aa3c89c77c1af2
Lemon Patwari <lemonpatwari@gmail.com>
2020-07-23 12:05:51 +0600

    employee employement exparience crud done

--- pages/my-jobs/edit-resume.vue
+++ pages/my-jobs/edit-resume.vue
@@ -672,17 +672,149 @@
                 </div>
 
 
-                <div class="tab-pane" id="employment" role="tabpanel">
-                  <p class="mb-0">
-                    Etsy mixtape wayfarers, ethical wes anderson tofu before they
-                    sold out mcsweeney's organic lomo retro fanny pack lo-fi
-                    farm-to-table readymade. Messenger bag gentrify pitchfork
-                    tattooed craft beer, iphone skateboard locavore carles etsy
-                    salvia banksy hoodie helvetica. DIY synth PBR banksy irony.
-                    Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh
-                    mi whatever gluten-free carles.
-                  </p>
+                <div class="tab-pane" id="employment" role="tabpanel3">
+                  <div class="card-body">
+                    <div id="accordion3">
+
+                      <div class="card mb-1 shadow-none">
+                        <div class="card-header" id="headingEmploymentHistory">
+                          <h6 class="m-0">
+                            <a href="#collapseEmploymentHistory" class="text-dark" data-toggle="collapse"
+                               aria-expanded="true"
+                               aria-controls="collapseEmploymentHistory">
+                              Employment History
+                            </a>
+                          </h6>
+                        </div>
+
+                        <div id="collapseEmploymentHistory" class="collapse"
+                             aria-labelledby="headingEmploymentHistory" data-parent="#accordion3">
+
+                          <div class="card-body">
+
+                            <div class="text-center">
+                              <button type="submit" class="btn btn-outline-secondary"
+                                      @click="addExperienceModal">Add Experience (If Required)
+                              </button>
+                            </div>
+
+                            <div class="table-responsive" v-if="employee.employment_histories.length > 0"
+                                 v-for="(row,index) in employee.employment_histories">
+
+                              <div class="col-lg-12 col-md-12 col-sm-12">
+                                <h5 class="text-muted">Experience {{ index+1 }}</h5>
+
+                                <div class="text-right" style="margin-top: -30px;">
+                                  <div class="btn-group">
+
+                                    <button type="submit" class="btn btn-outline-secondary btn-sm"
+                                            @click="editExperienceModal(row)"><i
+                                      class="bx bx-edit"></i> Edit
+                                    </button>
+
+                                    <button type="submit" class="btn btn-outline-warning btn-sm"
+                                            @click="deleteExperienceModal(row.id)"><i
+                                      class="bx bx-trash"></i> Delete
+                                    </button>
+
+                                  </div>
+                                </div>
+
+
+                              </div>
+
+
+                              <table class="table">
+                                <tr>
+                                  <th>Company Name <br>
+                                    <span>{{ row.company_name}}</span>
+                                  </th>
+
+                                  <th>Responsibilities <br>
+                                    <span>{{ row.responsibilities}}</span>
+                                  </th>
+                                </tr>
+
+                                <tr>
+                                  <th>Company Business <br>
+                                    <span>{{ row.company_business}}</span>
+                                  </th>
+
+                                  <th>Company Location<br>
+                                    <span>{{ row.company_location}}</span>
+                                  </th>
+                                </tr>
+
+                                <tr>
+                                  <th>Designation<br>
+                                    <span>{{ row.designation}}</span>
+                                  </th>
+
+                                  <th>Employment Period <br>
+                                    <span>{{ row.from_date }} to </span>
+
+                                    <span v-if="experience.currently_working == 1">Continue </span>
+                                    <span v-else>{{ row.to_date }}</span>
+
+                                  </th>
+                                </tr>
+
+                                <tr>
+                                  <th colspan="2">Department<br>
+                                    <span>{{ row.department}}</span>
+                                  </th>
+
+                                </tr>
+
+                                <tr>
+                                  <th colspan="2">Area Of Experiences<br>
+                                    <span>{{ row.area_of_experiences}}</span>
+                                  </th>
+
+                                </tr>
+
+                              </table>
+                            </div>
+                          </div>
+
+                        </div>
+                      </div>
+
+                      <div class="card mb-1 shadow-none">
+                        <div class="card-header" id="headingEmploymentHistoryArmy">
+                          <h6 class="m-0">
+                            <a href="#collapseEmploymentHistoryArmy" class="text-dark" data-toggle="collapse"
+                               aria-expanded="true"
+                               aria-controls="collapseEmploymentHistoryArmy">
+                              Employment History(For Retired Army Person)
+                            </a>
+                          </h6>
+                        </div>
+
+                        <div id="collapseEmploymentHistoryArmy" class="collapse"
+                             aria-labelledby="headingEmploymentHistoryArmy" data-parent="#accordion3">
+                          <div class="card-body">
+                            Army Anim pariatur cliche reprehenderit, enim eiusmod high life
+                            accusamus terry richardson ad squid. 3 wolf moon officia
+                            aute, non cupidatat skateboard dolor brunch. Food truck
+                            quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                            sunt aliqua put a bird on it squid single-origin coffee
+                            nulla assumenda shoreditch et. Nihil anim keffiyeh
+                            helvetica, craft beer labore wes anderson cred nesciunt
+                            sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                            Leggings occaecat craft beer farm-to-table, raw denim
+                            aesthetic synth nesciunt you probably haven't heard of them
+                            accusamus labore sustainable VHS.
+                          </div>
+                        </div>
+                      </div>
+
+
+                    </div>
+
+                  </div>
                 </div>
+
                 <div class="tab-pane" id="other_information" role="tabpanel">
                   <p class="mb-0">
                     Trust fund seitan letterpress, keytar raw denim keffiyeh etsy
@@ -694,6 +826,7 @@
                     mustache readymade keffiyeh craft.
                   </p>
                 </div>
+
                 <div class="tab-pane" id="photograph" role="tabpanel">
                   <p class="mb-0">
                     Trust fund seitan letterpress, keytar raw denim keffiyeh etsy
@@ -1662,6 +1795,143 @@
       </div>
     </div>
 
+    <!--addExperienceModal-->
+    <div class="modal fade" id="addExperienceModal" tabindex="-1" role="dialog"
+         aria-labelledby="addUserLabel"
+         aria-hidden="true">
+
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+
+            <h5 class="modal-title" v-if="!editMode">Add Experience</h5>
+            <h5 class="modal-title" v-else>Update Experience</h5>
+
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+
+          <form
+            @submit.prevent="editMode ? updateExperience() : createExperience()">
+            <div class="modal-body">
+              <div class="row">
+
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                  <div class="form-group">
+                    <label>Company Name</label>
+                    <input v-model="experience.company_name" type="text" name="company_name"
+                           placeholder="Enter company name"
+                           class="form-control"
+                           :class="{ 'is-invalid': experience.errors.has('company_name') }">
+                    <has-error :form="experience" field="company_name"></has-error>
+                  </div>
+
+                  <div class="form-group">
+                    <label>Company Business</label>
+                    <input v-model="experience.company_business" type="text" name="company_business"
+                           placeholder="Enter company business"
+                           class="form-control"
+                           :class="{ 'is-invalid': experience.errors.has('company_business') }">
+                    <has-error :form="experience" field="company_business"></has-error>
+                  </div>
+
+                  <div class="form-group">
+                    <label>Designation</label>
+                    <input v-model="experience.designation" type="text" name="designation"
+                           placeholder="Enter designation"
+                           class="form-control"
+                           :class="{ 'is-invalid': experience.errors.has('designation') }">
+                    <has-error :form="experience" field="designation"></has-error>
+                  </div>
+
+                  <div class="form-group">
+                    <label>Department</label>
+                    <input v-model="experience.department" type="text" name="department"
+                           placeholder="Enter department"
+                           class="form-control"
+                           :class="{ 'is-invalid': experience.errors.has('department') }">
+                    <has-error :form="experience" field="department"></has-error>
+                  </div>
+
+                  <div class="form-group">
+                    <label>Area Of Experiences</label>
+
+                    <textarea v-model="experience.area_of_experiences" name="area_of_experiences" cols="30" rows="2"
+                              class="form-control"
+                              :class="{ 'is-invalid': experience.errors.has('area_of_experiences') }" placeholder="Enter area of experiences"></textarea>
+
+                    <has-error :form="experience" field="area_of_experiences"></has-error>
+                  </div>
+                </div>
+
+
+                <div class="col-lg-6 col-md-6 col-sm-12">
+
+                  <div class="form-group">
+                    <label>Responsibilities</label>
+
+                    <textarea v-model="experience.responsibilities" name="responsibilities" cols="30" rows="4"
+                              class="form-control"
+                              :class="{ 'is-invalid': experience.errors.has('responsibilities') }" placeholder="Enter responsibilities"></textarea>
+
+                    <has-error :form="experience" field="responsibilities"></has-error>
+                  </div>
+
+                  <div class="form-group">
+                    <label>Company Location</label>
+                    <input v-model="experience.company_location" type="text" name="company_location"
+                           placeholder="Enter company location"
+                           class="form-control"
+                           :class="{ 'is-invalid': experience.errors.has('company_location') }">
+                    <has-error :form="experience" field="company_location"></has-error>
+                  </div>
+
+                  <div class="form-group">
+                    <label>From Date</label>
+
+                    <datepicker v-model="experience.from_date"
+                                :class="{ 'is-invalid': experience.errors.has('from_date') }"></datepicker>
+
+                    <has-error :form="experience" field="from_date"></has-error>
+                  </div>
+
+                  <div class="form-group">
+                    <label>To Date</label>
+
+                    <input type="text" value="Continuing" v-if="experience.currently_working == 1" readonly>
+
+                    <datepicker v-else v-model="experience.to_date"
+                                :class="{ 'is-invalid': experience.errors.has('to_date') }" required></datepicker>
+
+                    <has-error :form="experience" field="to_date"></has-error>
+                  </div>
+
+                  <div class="form-check mb-3">
+                    <input class="form-check-input" v-model="experience.currently_working" type="checkbox"
+                           name="currently_working" id="currently_working">
+                    <label class="form-check-label" for="currently_working">
+                      Currently Working
+                    </label>
+                  </div>
+
+                </div>
+
+
+              </div>
+            </div>
+
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              <button type="submit" v-show="!editMode" class="btn btn-success">Submit</button>
+              <button type="submit" v-show="editMode" class="btn btn-success">Update</button>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -1775,6 +2045,20 @@
           institute: '',
           duration: '',
           location: '',
+        }),
+
+        experience: new Form({
+          id: '',
+          company_name: '',
+          responsibilities: '',
+          company_business: '',
+          designation: '',
+          company_location: '',
+          department: '',
+          from_date: '',
+          to_date: '',
+          currently_working: '',
+          area_of_experiences: '',
         }),
 
         certificate: new Form({
@@ -2332,6 +2616,95 @@
           if (result.value) {
             var token = window.$nuxt.$cookies.get('token');
             this.certificate.delete(this.url + 'employee-professional-certification-summary/' + row + '?token=' + token).then(() => {
+              Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+              )
+              this.$emit('AfterDelete');
+            }).catch(() => {
+              Swal("Failed!", "There was something wrong.", "warning");
+            });
+          }
+        })
+      },
+      // addTrainingSummaries end
+
+      // addExperienceModal start
+      addExperienceModal() {
+        this.editMode = false;
+        this.experience.reset();
+        $('#addExperienceModal').modal('show');
+      },
+
+      editExperienceModal(row) {
+        this.editMode = true;
+        this.experience.reset();
+        $('#addExperienceModal').modal('show');
+        this.experience.fill(row);
+      },
+
+      createExperience() {
+        var token = window.$nuxt.$cookies.get('token');
+        this.experience.post(this.url + 'employee-employment-history?token=' + token)
+          .then(() => {
+
+            $('#addExperienceModal').modal('hide');
+
+            Toast.fire({
+              icon: 'success',
+              title: 'Successfully Submitted'
+            });
+
+            this.$emit('afterCreate');
+
+          })
+          .catch((error) => {
+
+            Toast.fire({
+              icon: 'warning',
+              title: 'There was something wrong'
+            });
+
+          })
+      },
+
+      updateExperience() {
+        var token = window.$nuxt.$cookies.get('token');
+        this.experience.put(this.url + 'employee-employment-history/' + this.experience.id + '?token=' + token)
+          .then(() => {
+
+            $('#addExperienceModal').modal('hide');
+
+            Toast.fire({
+              icon: 'success',
+              title: 'Updated successfully'
+            });
+
+            this.$emit('afterUpdate');
+
+          })
+          .catch(() => {
+            Toast.fire({
+              icon: 'warning',
+              title: 'There was something wrong'
+            });
+          })
+      },
+
+      deleteExperienceModal(row) {
+        Swal.fire({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+          if (result.value) {
+            var token = window.$nuxt.$cookies.get('token');
+            this.experience.delete(this.url + 'employee-employment-history/' + row + '?token=' + token).then(() => {
               Swal.fire(
                 'Deleted!',
                 'Your file has been deleted.',
