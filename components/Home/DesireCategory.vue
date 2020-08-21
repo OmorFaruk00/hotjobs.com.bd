82aeeb2c3ebe9c4b4f1b0b8e1645eb93957297a5
Lemon Patwari <lemonpatwari@gmail.com>
2020-08-21 15:35:40 +0600

    functional and industrial category show

--- components/Home/DesireCategory.vue
+++ components/Home/DesireCategory.vue
@@ -5,11 +5,55 @@
 
         <h1 class="card-title text-center">Choose Your Desire Category</h1>
 
-        <div class="row">
+
+        <div class="col-12 text-right">
+          <div class="btn-group" role="group" aria-label="Basic example">
+
+            <button @click="generalCategoryStep" type="button" class="btn btn-outline-info" :class="general_category_step ? 'active' : ''">Functional</button>
+
+            <button @click="industialsStep" type="button" class="btn btn-outline-secondary" :class="industials_step ? 'active' : ''">Industrial</button>
+
+          </div>
+        </div>
+
+
+        <div class="row" v-if="general_category_step">
+          <div v-for="row in general_category" class="col-lg-3 col-md-3 col-sm-12">
+            <div class="companies-item category-item-box">
+              <h3>
+                <a href="javaScript:void(0)">{{ row.name }}
+                  (
+                  <countTo :startVal='0' :endVal='test(row.id)' :duration='5000'></countTo>
+                  )
+                </a>
+              </h3>
+            </div>
+          </div>
+        </div>
+
+        <div class="row" v-if="industials_step">
+          <div v-for="row in industials" class="col-lg-3 col-md-3 col-sm-12">
+            <div class="companies-item category-item-box">
+              <h3>
+                <a href="javaScript:void(0)">{{ row.name }}
+                  (
+                  <countTo :startVal='0' :endVal='test(row.id)' :duration='5000'></countTo>
+                  )
+                </a>
+              </h3>
+            </div>
+          </div>
+
+        </div>
+
+        <!--<div class="row">
 
           <div class="col-sm-3 col-lg-3 category-border">
+
+
             <div class="category-item">
               <img src="../../static/images/box/8.png" alt="Job">
+
               <a href="#">Technical Support</a>
             </div>
           </div>
@@ -83,20 +127,99 @@
             </div>
           </div>
 
-        </div>
+        </div>-->
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "DesireCategory"
+  import countTo from 'vue-count-to';
+
+  export default {
+    name: "DesireCategory",
+    components: {
+      countTo,
+    },
+
+    data() {
+      return {
+        general_category_step:true,
+        industials_step:false,
+        url: this.$axios.defaults.baseURL,
+        general_category: '',
+        industials: '',
+      }
+    },
+
+    methods: {
+      fetchGeneralCategory() {
+        var vm = this;
+        this.$axios.get('skill-general-category').then(function (response) {
+
+          vm.general_category = response.data;
+
+        }).catch(function (error) {
+
+          Toast.fire({
+            icon: 'warning',
+            title: 'There was something wrong'
+          });
+
+        });
+      },
+
+      fetchIndustryCategory() {
+        var vm = this;
+        this.$axios.get('industry-category-lists').then(function (response) {
+
+          vm.industials = response.data;
+
+        }).catch(function (error) {
+
+          Toast.fire({
+            icon: 'warning',
+            title: 'There was something wrong'
+          });
+
+        });
+      },
+
+      generalCategoryStep(){
+        this.general_category_step = true;
+        this.industials_step = false;
+      },
+
+      industialsStep(){
+        this.industials_step = true;
+        this.general_category_step = false;
+      },
+
+      test(id) {
+        return 100;
+      }
+    },
+
+    beforeMount() {
+
+      this.fetchGeneralCategory();
+      this.fetchIndustryCategory();
+
     }
+  }
 </script>
 
 <style scoped>
   h1 {
     font-size: 35px;
+  }
+
+  .category-item-box {
+    padding: 15px 0px 1px 0px;
+  }
+
+  .category-item-box h3 a {
+    font-size: 14px;
   }
 </style>
