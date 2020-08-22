cc9ab24c989f3a1a885c0364846e3045aff59feb
Lemon Patwari <lemonpatwari@gmail.com>
2020-08-22 12:55:09 +0600

    special skill show done

--- components/Home/Skill.vue
+++ components/Home/Skill.vue
@@ -6,6 +6,22 @@
         <h1 class="card-title text-center">Special Skilled Jobs</h1>
 
         <div class="row">
+          <div v-for="row in special_categories" class="col-lg-3 col-md-3 col-sm-12">
+            <div class="companies-item category-item-box">
+              <h3>
+                <a href="javaScript:void(0)">{{ row.name }}
+                  (
+                  <countTo :startVal='0' :endVal='row.current_job_posts.length'
+                           :duration='5000'></countTo>
+                  )
+                </a>
+              </h3>
+            </div>
+          </div>
+
+        </div>
+
+        <!--<div class="row">
 
           <div class="col-sm-3 col-lg-3 category-border">
             <div class="category-item">
@@ -83,20 +99,61 @@
             </div>
           </div>
 
-        </div>
+        </div>-->
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import countTo from 'vue-count-to';
     export default {
-        name: "Skill"
+        name: "Skill",
+      components: {
+        countTo,
+      },
+      data() {
+        return {
+          url: this.$axios.defaults.baseURL,
+          special_categories:'',
+        }
+      },
+
+      methods:{
+        fetchSpecialCategory() {
+          var vm = this;
+          this.$axios.get('skill-special-category').then(function (response) {
+
+            vm.special_categories = response.data;
+
+          }).catch(function (error) {
+
+            Toast.fire({
+              icon: 'warning',
+              title: 'There was something wrong'
+            });
+
+          });
+        },
+      },
+
+      beforeMount() {
+        this.fetchSpecialCategory();
+      }
     }
 </script>
 
 <style scoped>
   h1 {
     font-size: 35px;
+  }
+
+  .category-item-box {
+    padding: 15px 0px 1px 0px;
+  }
+
+  .category-item-box h3 a {
+    font-size: 14px;
   }
 </style>
