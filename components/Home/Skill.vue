4c15482ec9b58bcb350b0cee69df66784444734b
Lemon Patwari <lemonpatwari@gmail.com>
2020-08-25 17:13:06 +0600

    special category skill job show

--- components/Home/Skill.vue
+++ components/Home/Skill.vue
@@ -9,7 +9,7 @@
           <div v-for="row in special_categories" class="col-lg-3 col-md-3 col-sm-12">
             <div class="companies-item category-item-box">
               <h3>
-                <a href="javaScript:void(0)">{{ row.name }}
+                <a href="javaScript:void(0)" @click="fetchJob(row.id)">{{ row.name }}
                   (
                   <countTo :startVal='0' :endVal='row.current_job_posts.length'
                            :duration='5000'></countTo>
@@ -136,6 +136,13 @@
 
           });
         },
+
+        fetchJob(id){
+          var id = id;
+          this.$router.push(`/jobsearch/${id}`)
+
+        }
+        
       },
 
       beforeMount() {
