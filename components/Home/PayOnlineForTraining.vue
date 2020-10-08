2fc93f7c7ff321b8d3aa5b6ac0e344c407476a24
Lemon Patwari <lemonpatwari@gmail.com>
2020-10-08 14:05:30 +0600

    pay online section design modified

--- components/Home/PayOnlineForTraining.vue
+++ components/Home/PayOnlineForTraining.vue
@@ -8,45 +8,77 @@
         <div class="training-price">
           <div class="row">
 
-            <div class="col-lg-6 mix web ui">
+            <div class="col-lg-6 mix web ui mb-2">
               <div class="job-item training-price-item price-left-side">
-                <img src="../../static/images/box/1.png" alt="Job">
-                <div class="job-inner align-items-center">
-                  <div class="training-price-inner-left">
-                    <a href="company-details.html">Post Budget Discussion on VAT & TAX Changes 2020</a>
-                    <p>3 - 4 Jul 2020</p>
+                <div class="row">
+                  <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 text-center">
+                    <img src="../../static/images/box/1.png" alt="Job">
                   </div>
-                </div>
-              </div>
 
-              <div class="job-item training-price-item price-left-side">
-                <img src="../../static/images/box/1.png" alt="Job">
-                <div class="job-inner align-items-center">
-                  <div class="training-price-inner-left">
-                    <a href="company-details.html">Post Budget Discussion on VAT & TAX Changes 2020</a>
-                    <p>3 - 4 Jul 2020</p>
+                  <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                    <div class="job-inner align-items-center">
+                      <div class="training-price-inner-left">
+                        <a href="javaScript:void(0)">Post Budget Discussion on VAT & TAX Changes 2020</a>
+                        <p>3 - 4 Jul 2020</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div class="col-lg-6 mix web ui">
+            <div class="col-lg-6 mix web ui mb-2">
               <div class="job-item training-price-item price-left-side">
-                <img src="../../static/images/box/1.png" alt="Job">
-                <div class="job-inner align-items-center">
-                  <div class="training-price-inner-left">
-                    <a href="company-details.html">Post Budget Discussion on VAT & TAX Changes 2020</a>
-                    <p>3 - 4 Jul 2020</p>
+                <div class="row">
+                  <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 text-center">
+                    <img src="../../static/images/box/1.png" alt="Job">
+                  </div>
+
+                  <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                    <div class="job-inner align-items-center">
+                      <div class="training-price-inner-left">
+                        <a href="javaScript:void(0)">Post Budget Discussion on VAT & TAX Changes 2020</a>
+                        <p>3 - 4 Jul 2020</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
+            </div>
 
+            <div class="col-lg-6 mix web ui mb-2">
               <div class="job-item training-price-item price-left-side">
-                <img src="../../static/images/box/1.png" alt="Job">
-                <div class="job-inner align-items-center">
-                  <div class="training-price-inner-left">
-                    <a href="company-details.html">Post Budget Discussion on VAT & TAX Changes 2020</a>
-                    <p>3 - 4 Jul 2020</p>
+                <div class="row">
+                  <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 text-center">
+                    <img src="../../static/images/box/1.png" alt="Job">
+                  </div>
+
+                  <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                    <div class="job-inner align-items-center">
+                      <div class="training-price-inner-left">
+                        <a href="javaScript:void(0)">Post Budget Discussion on VAT & TAX Changes 2020</a>
+                        <p>3 - 4 Jul 2020</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-6 mix web ui mb-2">
+              <div class="job-item training-price-item price-left-side">
+                <div class="row">
+                  <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 text-center">
+                    <img src="../../static/images/box/1.png" alt="Job">
+                  </div>
+
+                  <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                    <div class="job-inner align-items-center">
+                      <div class="training-price-inner-left">
+                        <a href="javaScript:void(0)">Post Budget Discussion on VAT & TAX Changes 2020</a>
+                        <p>3 - 4 Jul 2020</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
