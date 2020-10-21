f2d1de53eb93e1aad56fab50f18845b6b96ac272
Lemon Patwari <lemonpatwari@gmail.com>
2020-10-21 18:22:49 +0600

    error page 404 create

--- nuxt.config.js
+++ nuxt.config.js
@@ -221,7 +221,7 @@ export default {
   },
 
   generate: {
-    fallback: false, // if you want to use '404.html' instead of the default '200.html'
+    fallback: true, // if you want to use '404.html' instead of the default '200.html'
   }
 
 }
