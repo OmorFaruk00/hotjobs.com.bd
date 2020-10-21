71e3995e31a056cb88ae1fc5654c133c713bd75d
Lemon Patwari <lemonpatwari@gmail.com>
2020-10-21 18:14:04 +0600

    toasr add

--- pages/job-search/_id/_slug/index.vue
+++ pages/job-search/_id/_slug/index.vue
@@ -144,6 +144,20 @@
 </template>
 
 <script>
+
+import Swal from 'sweetalert2'
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  onOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
 export default {
   name: "index",
   validate({params}) {
