f4124133dbe3a68ca9051102e24e4ce1db19c4dc
Lemon Patwari <lemonpatwari@gmail.com>
2020-10-23 14:26:33 +0600

    custom button modified and sub manu active add

--- components/Employer/SubNavbar.vue
+++ components/Employer/SubNavbar.vue
@@ -2,10 +2,10 @@
   <div class="row mt-2">
 
     <div class="col-lg-12 text-right">
-
-      <button class="btn btn-info"><i class="bx bx-search-alt"></i> Search CV Bank</button>
-      <nuxt-link to="/employer/job-posting-board" class="btn btn-success"><i class="bx bxs-plus-circle"></i> Post a New Job</nuxt-link>
-      <nuxt-link to="/employer/tutor-request" class="btn btn-success"><i class="bx bxs-plus-circle"></i> Tutor Request</nuxt-link>
+      <nuxt-link to="/employer/dashboard" class="tcb-animate-e tcb-default"><i class="bx bx-home-circle"></i> Dashboard <i v-if="$nuxt.$route.name == 'employer-dashboard'" class="bx bx-check-circle"></i></nuxt-link>
+<!--      <button class="tcb-animate-e tcb-info"><i class="bx bx-search-alt"></i> Search CV Bank</button>-->
+      <nuxt-link to="/employer/job-posting-board" class="tcb-animate-e tcb-success"><i class="bx bxs-plus-circle"></i> Post a New Job <i v-if="$nuxt.$route.name == 'employer-job-posting-board'" class="bx bx-check-circle"></i></nuxt-link>
+      <nuxt-link to="/employer/tutor-request" class="tcb-animate-e tcb-success"><i class="bx bxs-plus-circle"></i> Tutor Request <i v-if="$nuxt.$route.name == 'employer-tutor-request'" class="bx bx-check-circle"></i></nuxt-link>
 
     </div>
 
